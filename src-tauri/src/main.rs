c9a14ed5d677a15b305ee7bb8caa9ef7cfb7e84d
amason <ammason12@gmail.com>
2022-09-20 19:37:19 -0700

    add prisma client rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,27 @@
     windows_subsystem = "windows"
 )]
 
+mod prisma;
+use prisma::PrismaClient;
+use prisma_client_rust::NewClientError;
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let client_result: Result<PrismaClient, NewClientError> = prisma::new_client().await;
+    let _ = client_result.expect("Failed to setup prisma client");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|_app| {
+            println!("Setting up");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
